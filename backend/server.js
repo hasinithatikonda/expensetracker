6cade7898703d54312ba6d5af730b39a7993b027
hasinithatikonda <23eg106d22@anurag.edu.in>
2026-05-31 20:22:12 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,9 @@ const corsOptions = {
     const allowed = [
       process.env.FRONTEND_URL,
       'http://localhost:3000',
+      'http://localhost:3002',
       'http://127.0.0.1:3000',
+      'http://127.0.0.1:3002',
     ].filter(Boolean);
     if (allowed.includes(origin) || /\.vercel\.app$/i.test(origin)) {
       return callback(null, true);
